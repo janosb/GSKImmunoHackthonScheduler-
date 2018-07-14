b58ab3cf104bdfa211f534b3c90dba50f971f66e
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-07-14 01:21:48 -0400

    updated git ignore and added empty API project

---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Gsk.Hack.Schedule.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gsk.Hack.Schedule.API.WebApiApplication" Language="C#" %>
