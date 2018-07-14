e74f8cb1159e0da28b8b1e92aabdba1c2428ea7b
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-07-14 14:55:43 -0400

    Added simple api endpoint with sample resonse

---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Controllers/RecommendController.cs
@@ -0,0 +1,28 @@
+﻿using Gsk.Hack.Schedule.API.Models;
+using System;
+using System.Web.Http;
+
+namespace Gsk.Hack.Schedule.API.Controllers
+{
+    [RoutePrefix("api/schedule")]
+    public class ScheduleController : ApiController
+    {
+        [HttpPost]
+        [Route("recommend")]
+        public IHttpActionResult Recommend(RecommendRequest request)
+        {
+            DateTime appointmentTime;
+
+            if (DateTime.TryParse(request.ApointmentTime, out appointmentTime))
+            {
+                return Ok(request.CallerName + " made an appointment for " + request.PatientName + " at " 
+                + appointmentTime.Hour + " O'Clock on " +
+                appointmentTime.DayOfWeek + " " + appointmentTime.Month + " " + appointmentTime.Day);
+            }
+            else
+            {
+                return BadRequest("Could not parse date time");
+            }  
+        }
+    }
+}
---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Models/RecommendRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gsk.Hack.Schedule.API.Models
+{
+    public class RecommendRequest
+    {
+        public string CallerName { get; set; }
+        public string PatientName { get; set; }
+        public string ApointmentTime { get; set; }
+    }
+}
