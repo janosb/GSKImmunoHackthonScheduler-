8cf8a724d4d19fecc646fc07a9680e24b23161da
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-07-14 16:17:00 -0400

    Added simple repo access and unit test

---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API.Tests/Repositories/RepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Gsk.Hack.Schedule.API.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gsk.Hack.Schedule.API.Tests.Repositories
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void GetVaccines_ReadsFromTestTable_ShouldSucceed()
+        {
+            MySqlRepository repository = new MySqlRepository();
+            repository.GetVaccines("Pat");
+        }
+    }
+}
---
+++ server/Gsk.Hack.Schedule.API/Gsk.Hack.Schedule.API/Repositories/MySqlRepository.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gsk.Hack.Schedule.API.Repositories
+{
+    public class MySqlRepository
+    {
+        private const string ConnectionString =
+            "Server=gskdbserverimnno.mysql.database.azure.com; Port=3306; Database=vaccine; Uid=gskadmin@gskdbserverimnno; Pwd=ImmunoH@CK1;";
+
+        public MySqlRepository()
+        {
+            
+        }
+
+        public void GetVaccines(string name)
+        {
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+            string sql = "SELECT * FROM TEST";
+            //string sql = "SELECT * FROM TEST WHERE Name='@Name'";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            //cmd.Parameters.AddWithValue("@Name", name);
+
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+            }
+            rdr.Close();
+        }
+    }
+}
