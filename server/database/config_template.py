b969a938fe4d02d08219005ed9b750bc11427236
janos_botyanszki <janos@schireson.com>
2018-07-14 17:39:43 -0400

    adding config template

---
+++ server/database/config_template.py
@@ -0,0 +1,22 @@
+db_name = 'vaccine_clinic.db'
+
+mysql_db = ''
+mysql_host = ''
+mysql_user = ''
+mysql_pass = ''
+
+vaccine_logic_file = ''
+vaccine_logic_table_name = 'vaccine_recommendations'
+
+vaccine_demos_file = ''
+vaccine_demos_table_name = 'demos'
+
+vaccine_data_file = ''
+vaccine_data_table_name = 'vaccine_data'
+
+patients_data_file = ''
+patients_table_name = 'patients'
+
+schedule_data_file = ''
+schedule_table_name = 'schedule'
+
